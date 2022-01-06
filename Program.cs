2fe80cc9c06e099573901d8657f08045b5ef19b9
MelihOmer <97173932+MelihOmer@users.noreply.github.com>
2022-01-06 22:12:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Console_Programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Adınızı Giriniz");
+            string name = Console.ReadLine();
+            Console.Write("Soyadınızı Giriniz");
+            string Soyad = Console.ReadLine();
+
+            Console.Write("Merhaba " + name +" "+ Soyad);
+
+        }
+    }
+}
+
